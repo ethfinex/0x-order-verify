92c330b9effd9cb1ee88b15aa99c0d1c6665a17f
brentkirkland <brentckirkland@gmail.com>
2017-11-01 18:12:06 -0700

    new look, new feel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { ZeroEx } from '0x.js'
+import Textarea from 'react-textarea-autosize'
 
 import logo from './logo.svg'
 import './App.css'
@@ -9,7 +10,9 @@ class App extends Component {
     super(props)
     this.state = {
       order: '',
-      hash: ''
+      hash: '',
+      success: false,
+      error: ''
     }
 
     this.handleOrderChange = this.handleOrderChange.bind(this)
@@ -18,43 +21,140 @@ class App extends Component {
   }
 
   handleOrderChange (e) {
-    this.setState({order: e.target.value})
+    let parsedJson = e.target.value
+    let error = null
+    try {
+      parsedJson = JSON.parse(e.target.value)
+    } catch (e) {
+      // silence error, user could be typing
+      error = e
+    }
+    this.setState({
+      order: (error) ? parsedJson : JSON.stringify(parsedJson, undefined, 2),
+      error: '',
+      success: false
+    })
   }
 
   handleHashChange (e) {
-    this.setState({hash: e.target.value})
+    this.setState({
+      hash: e.target.value,
+      error: '',
+      success: false
+    })
   }
 
   handleVerify () {
     if (this.state.order === '') {
-      window.alert('Please first paste in a 0x order')
+      this.setState({
+        error: 'Please first paste in a 0x order.'
+      })
       return
     } else if (this.state.hash === '') {
-      window.alert('Please first paste in a 0x hash')
+      this.setState({
+        error: 'Please first paste in a 0x hash.'
+      })
       return
     }
-    console.log('Order: ' + this.state.order)
-    console.log('Hash: ' + this.state.hash)
-    console.log('Starting verification')
 
     try {
       JSON.parse(this.state.order)
     } catch (e) {
-      window.alert('Order must be in JSON format i.e. {\'key\':\'value\',...}')
+      this.setState({
+        error: 'Order must be in JSON format i.e. {\'key\':\'value\',...}'
+      })
       return
     }
 
     let calculatedOrderHash =
       ZeroEx.getOrderHashHex(JSON.parse(this.state.order))
-    console.log(calculatedOrderHash)
     if (ZeroEx.isValidOrderHash(this.state.hash)) {
       if (calculatedOrderHash === this.state.hash) {
-        window.alert('The hash is correct!')
+        this.setState({success: true})
       } else {
-        window.alert('The hash does not match the order')
+        this.setState({
+          error: 'The hash does not match the order.'
+        })
       }
     } else {
-      window.alert('The hash should look be in the following format 0x4jdsf...')
+      this.setState({
+        error: 'The hash should look be in the following format 0x4jdsf...'
+      })
+    }
+  }
+
+  renderTextAreaDiv (params = {}) {
+    const {
+      _text,
+      _className,
+      _onChange,
+      _value,
+      _placeholder
+    } = params
+    return (
+      <div className='App-div-input'>
+        <b>{_text}</b>
+        <Textarea
+          className={_className}
+          onChange={_onChange}
+          value={_value}
+          placeholder={_placeholder}
+          style={{'padding': '20px'}}
+          />
+      </div>
+    )
+  }
+
+  renderForm () {
+    const orderParam = {
+      _text: 'Order parameters (JSON format):',
+      _className: 'App-textarea',
+      _onChange: this.handleOrderChange,
+      _value: this.state.order,
+      _placeholder: '{"expirationUnixTimestampSec": 1507056275, ' +
+        '"feeRecipient": 0xe73a1998ce936acebeb7899...'
+    }
+    const hashParam = {
+      _text: 'Hashed order (to verify before signing):',
+      _className: 'App-textarea',
+      _onChange: this.handleHashChange,
+      _value: this.state.hash,
+      _placeholder: '0xf53a1j98ce6993dxebeb7899...'
+    }
+    return (
+      <div className='App-body-container'>
+        <form className='App-form'>
+          {this.renderTextAreaDiv(orderParam)}
+          {this.renderTextAreaDiv(hashParam)}
+          <div className='App-div-submit'>
+            <input
+              type={'button'}
+              className={'App-button'}
+              value={'Verify'}
+              onClick={this.handleVerify} />
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  renderAlert () {
+    if (this.state.success) {
+      return (
+        <div className='App-body-container'>
+          <div className='App-alert-green'>
+            <div>The hash is correct!</div>
+          </div>
+        </div>
+      )
+    } else if (this.state.error !== '') {
+      return (
+        <div className='App-body-container'>
+          <div className='App-alert-red'>
+            <div>{this.state.error}</div>
+          </div>
+        </div>
+      )
     }
   }
 
@@ -63,59 +163,26 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>0x Order Verification Tool</h1>
+          <h1 className='App-title'><b>0x</b> Order Verification Tool</h1>
         </header>
-        <p className='App-intro'>
-          To verify that a hash is correct before signing it,
-          copy a 0x order and hash into the boxes below:
-        </p>
-        <br />
-        <br />
-        <form>
-          <b>Order parameters (JSON format):</b>
-          <br />
-          <input
-            type='textarea'
-            className='App-inputbox'
-            onChange={this.handleOrderChange}
-            value={this.state.order}
-            placeholder={'{"expirationUnixTimestampSec": 1507056275,' +
-              '"feeRecipient": 0xe73a1998ce936acebeb7899...'} />
-          <br />
-          <br />
-          <b>Hashed order (to verify before signing):</b>
-          <br />
-          <input
-            type='text'
-            className='App-inputbox'
-            onChange={this.handleHashChange}
-            value={this.state.hash}
-            placeholder='0xf53a1j98ce6993dxebeb7899...' />
-          <br />
-          <br />
-          <input
-            type='submit'
-            className='App-button'
-            value='Verify'
-            onClick={this.handleVerify} />
-        </form>
-        <div className='App-efx'>
-          This tool was created open source by &thinsp;
-          <a href='https://www.ethfinex.com'>Ethfinex</a>
-          &thinsp; and the source code can be found on &thinsp;
-          <a href='https://github.com/plutoegg/0x-order-verify'>
-            Github
-          </a>
-        </div>
-        <br />
-        <div className='App-efxx'>
-          This tool was created open source by &thinsp;
-           <a href='https://www.ethfinex.com'>Ethfinex</a>
-          &thinsp; and the source code can be found on &thinsp;
-           <a href='https://github.com/plutoegg/0x-order-verify'>
-            Github
-          </a>
-          .
+        <div className='App-body'>
+          <div className='App-body-container'>
+            <p className='App-intro'>
+              To verify that a hash is correct before signing it,
+              copy a 0x order and hash into the boxes below:
+            </p>
+          </div>
+          {this.renderAlert()}
+          {this.renderForm()}
+          <div className='App-body-container'>
+            This tool was created open source by &thinsp;
+            <a href='https://www.ethfinex.com'>Ethfinex</a>
+            &thinsp; and the source code can be found on &thinsp;
+            <a href='https://github.com/plutoegg/0x-order-verify'>
+              Github
+            </a>
+            .
+          </div>
         </div>
       </div>
     )
